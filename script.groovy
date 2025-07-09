fcdf9083e0be30d32323b48f95d2db5149135b3c
Mohamed Diaa <48259373+MoDiaa@users.noreply.github.com>
2025-07-09 10:57:23 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
         sh 'docker build -t mohameddiaa10/myrepo:gradle-simple-app .'
-        sh "echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin"
+        sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
         sh 'docker push mohameddiaa10/myrepo:gradle-simple-app'
     }
 } 
