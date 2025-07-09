dbd85413708d10f72ba9d68df993ce5d52840808
Mohamed Diaa <48259373+MoDiaa@users.noreply.github.com>
2025-07-09 11:52:40 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,11 +6,11 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-        sh 'docker build -t mohameddiaa10/myrepo:gradle-simple-app-Diaa .'
-        sh 'echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin'
-        sh 'docker push mohameddiaa10/myrepo:gradle-simple-app-Diaa'
+        sh 'docker build -t mohameddiaa10/myrepo:gradle-simple-app .'
+        sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
+        sh 'docker push mohameddiaa10/myrepo:gradle-simple-app'
     }
-} 
+}  
 
 def deployApp() {
     echo 'deploying the application...'
